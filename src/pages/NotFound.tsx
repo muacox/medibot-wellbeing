ecd9e60e552f288e4846e60ffd1ca6f1d77bbf7d
muacox <pilhamoneystrip@gmail.com>
2026-04-25 19:54:03 +0100

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -11,9 +11,9 @@ const NotFound = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-muted">
       <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold">404</h1>
-        <p className="mb-4 text-xl text-muted-foreground">Oops! Page not found</p>
-        <a href="/" className="text-primary underline hover:text-primary/90">
+        <h1 className="mb-4 text-4xl font-bold">PÁGINA NÃO EXISTE</h1>
+        <p className="mb-4 text-xl text-muted-foreground">INFELIZMENTE ESSA PÁGINA NÃO EXISTE</p>
+        <a href="/" className="text-primary underline hover:text-primary/100">
           Return to Home
         </a>
       </div>
